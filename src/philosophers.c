2bcf016258864df880f861e8ffcda5fb509215e7
christie26 <yoonseo9800@gmail.com>
2023-03-11 22:36:40 +0900

    eat with two fork! eat and sleep? and data race happens

--- src/philosophers.c
+++ src/philosophers.c
@@ -12,19 +12,86 @@
 
 #include "../include/philo.h"
 
-void *each_philo(void *data)
+void	philo_init(t_philo *philo)
+{
+	int	i;
+	int	num;
+
+	i = philo->id;
+	num = philo->arg->num;
+	if (num % 2 == 0)
+	{
+		if (i % 2 == 0)
+		{
+			pthread_mutex_lock(philo->left->mutex);
+			pthread_mutex_lock(philo->right->mutex);
+			philo->left->fork = 1;
+			philo->right->fork = 1;
+			printf("%d start eating\n", philo->id);
+			
+			philo->status = EAT;
+		}
+	}
+	else
+	{
+		if (i % 2 == 0 && i != num - 1)
+		{
+			pthread_mutex_lock(philo->left->mutex);
+			pthread_mutex_lock(philo->right->mutex);
+			philo->left->fork = 1;
+			philo->right->fork = 1;
+			printf("%d start eating\n", philo->id);
+			philo->status = EAT;
+		}
+	}	
+}
+
+void	philo_eat(t_philo *philo)
+{
+
+	while (philo->status != EAT)
+	{
+		sleep(3);
+		if (philo->left->fork == 0)
+		{
+			pthread_mutex_lock(philo->left->mutex);
+			philo->left->fork = 1;
+		}
+		if (philo->right->fork == 0)
+		{
+			pthread_mutex_lock(philo->right->mutex);
+			philo->right->fork = 1;
+		}
+		if (philo->left->fork && philo->right->fork)
+		{
+			printf("%d start eating\n", philo->id);
+			philo->status = EAT;
+		}
+	}
+
+	usleep(philo->arg->eat);	// eating
+	printf("%d finish eating\n", philo->id);
+	philo->left->fork = 0;	
+	philo->right->fork = 0;	
+	pthread_mutex_unlock(philo->left->mutex);
+	pthread_mutex_unlock(philo->right->mutex);
+
+	philo->status = THINK;
+	usleep(100);
+
+}
+
+void	*each_philo(void *data)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)(data);
 	printf("I am %dth philosophers\n", philo->id);
 
-	pthread_mutex_lock(philo->left->mutex);
-	printf("Philosopher %d is using left fork\n", philo->id + 1);
-	pthread_mutex_unlock(philo->left->mutex);
-
-	pthread_mutex_lock(philo->right->mutex);
-	printf("Philosopher %d is using right fork\n", philo->id + 1);
-	pthread_mutex_unlock(philo->right->mutex);
+	philo_init(philo);
+	while (philo->status != DIED)
+	{
+		philo_eat(philo);	
+	}	
 	return ((void *)(long)(philo->id));
 }
